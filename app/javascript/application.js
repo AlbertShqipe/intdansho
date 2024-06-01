baf2340425b657a6bc5ad230495345670ddc1644
Albert Nikolli <albert_nikolli@icloud.com>
2024-06-01 08:38:48 +0200

    app ready

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,25 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+document.addEventListener("DOMContentLoaded", function() {
+  var logo = document.getElementById('logo');
+  // current code is used to get the current position of the scroll in the window
+  window.addEventListener('scroll', function() {
+    // get the current position of the window based on the scrolling
+    var currScrollPos2 = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    // select the div to reduce or augment the opacity based on the position of the scrollbar
+    const logoDiv = this.document.getElementById('logo');
+    logoDiv.style.opacity = (-currScrollPos2 / 300 + 1);
+
+    // get the navbar element
+    const navbar = document.getElementById('navbar');
+    if (currScrollPos2 == 660 || currScrollPos2 > 660 ) {
+      navbar.classList = "navbar-660";
+    } else  {
+    // we select again the element since the class has changed to navbar-660
+      navbar.classList = "navbar";
+    };
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+
+  get "accueil" => "pages#accueil"
+  get "services" => "pages#services"
+  get "media" => "pages#media"
+  get "notrehistoire" => "pages#notrehistoire"
+  get "contact" => "pages#contact"
 
   # Defines the root path route ("/")
   # root "posts#index"
