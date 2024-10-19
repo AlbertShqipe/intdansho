19de6a5a6a8df3d6684556d5a2b25d5d3d33f0dc
Albert Nikolli <albert_nikolli@icloud.com>
2024-10-19 13:27:50 +0200

    added all the necessary changes asked by anais

--- app/controllers/professeurs_controller.rb
+++ app/controllers/professeurs_controller.rb
@@ -3,10 +3,10 @@ class ProfesseursController < ApplicationController
     @teachers = teachers = [
       { name: "SARKIS GRIGORIAN", position: "Danse Classique FONDATEUR", picture: "sakoPortrait.png" },
       { name: "DENIS LAMAJ", position: "Danse Classique et Barre au Sol", picture: "denisPortrait.png"},
-      { name: "GAEL ALAMARGOT", position: "Danse Classique et contemporaine", picture: "gaelPortrait.png"},
+      { name: "GAEL ALAMARGOT", position: "Danse Classique et Contemporaine", picture: "gaelPortrait.png"},
       { name: "PETROS CHROKOYAN", position: "Danse Classique et Fitness Danse", picture: "petrosPortrait.png"},
       { name: "NASIM", position: "Breakdance et Hip Hop", picture: "nasim.jpg" },
-      { name: "ANAIS GRIGORIAN KOTANDJIAN", position: "Éveil / Initiation FONDATRICE", picture: "anaisPortrait1.png"},
+      { name: "ANAIS GRIGORIAN KOTANDJIAN", position: "FONDATRICE", picture: "anaisPortrait1.png"},
       { name: "ANNA GRIGORIAN", position: "Danse Classique et Moderne", picture: "anna.jpg"},
       { name: "ALMENDRA NAVARRO", position: "Danse Classique et Barre au Sol", picture: "almendraPortrait.png"},
       { name: "TERESA GRIGORIAN", position: "Danse du Monde", picture: "theresa.JPG"},
