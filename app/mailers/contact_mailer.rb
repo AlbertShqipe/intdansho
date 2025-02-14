ca04ec7214a59fb1a59d1b395b65f677e507e3d8
Albert Nikolli <albert_nikolli@icloud.com>
2025-02-14 17:47:01 +0100

    added the email to the application

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,14 @@
+class ContactMailer < ApplicationMailer
+  default to: "internationaldanceshow@gmail.com" # This is the email address that the contact form will be sent to
+
+  def contact_email(name, email, message)
+    @name = name
+    @message = message
+    @email = email
+
+    mail(
+      from: 'no-reply@internationaldanceshow.com',
+      subject: "Nouveau message de #{@name}, #{@email}"
+    )
+  end
+end
